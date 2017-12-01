995a5685c14cc9e4b2b467ada95ada6fb408fb8d
Sarar Aseer <saseer@stuy.edu>
2017-11-30 22:33:02 -0500

    Attempted first step

--- 06SuperArrayIterator/SuperArrayIterator.java
+++ 06SuperArrayIterator/SuperArrayIterator.java
@@ -1,9 +1,22 @@
-public class SuperArrayIterator extends SuperArray  implements Iterator<String>{
+import java.util.Iterator;
 
 
+public class SuperArrayIterator extends SuperArray implements Iterator<String>{
+    private SuperArray sa;
+    private int index;
 
+    public boolean hasNext(){
+	return false;
+    }
+    
+    public String next(){
+	return null;
+    }
 
-
+    public void remove(){
+	throw new UnsupportedOperationException("not gonna work");	       
+    }
+    
 
 
 
