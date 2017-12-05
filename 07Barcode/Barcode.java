792704e4e88c2320064eece7d5c5cf7fd47ca0b2
Sarar Aseer <saseer@stuy.edu>
2017-12-04 22:48:55 -0500

    Created and finished and tested toCode

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -1,31 +1,37 @@
-
-
 public class Barcode{
     private String  zip;
     private int check;
     private String Barcode;
     private String[] key={"||:::", ":::||", "::|:|", "::||:" , ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::"};
-
-
+    
+    
     public Barcode(String zip) {
 	this.zip=zip;
-	for (int i = 0 ; i < zip.length() ; i++){
-	    check+=Integer.parseInt(zip.substring(i,i+1));
+	if (zip.length()==32){
+	    for (int i = 0 ; i < zip.length() ; i++){
+		try{
+		    check+=Integer.parseInt(zip.substring(i,i+1));}
+		catch(IllegalArgumentException e) {
+		    throw new IllegalArgumentException("Threw an IllegalArgumentException");
+			
 		}
+	    }
+	}
+	
     }
 
 
     public String toString(){
-	return Barcode+","+zip;
+	return Barcode+" ( "+zip+")";
 
 
     }
 
-    public String getzip(){
+    public String getZip(){
 	return zip;
     }
 
-    public String getcode(){
+    public String getCode(){
 	Barcode="|";
 	for (int i=0; i < zip.length()-1 ; i++){
 	    Barcode+=key[Integer.parseInt(zip.substring(i,i+1))];
@@ -36,6 +42,24 @@ public class Barcode{
 	return Barcode;
 		
     }
+    
+    public String toZip(String input){
+	String test= input.substring(1,26);
+	String code="";
+	for (int i= 0; i < test.length(); i++){
+	    for (int x=0; x <key.length ; x++){
+		if(key[x].equals(test.substring(i,i+5))){
+		    code+=""+x;
+		}
+	    }
+	    i+=4;
+	}
+	return code;
+
+    }
+    
+			    
+		  
 
     public boolean equals(Barcode other){
 	return other.equals(zip);
@@ -43,7 +67,7 @@ public class Barcode{
 
     public int compareTo(Barcode other){
 
-	return Integer.parseInt(zip)-Integer.parseInt(other.getzip());
+	return Integer.parseInt(zip)-Integer.parseInt(other.getZip());
     }
 
 
